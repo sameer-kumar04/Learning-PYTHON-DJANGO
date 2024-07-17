e76a49cfa816a9882de2e17587d10891835ed44d
sameer-kumar04 <whoissameer04@gmail.com>
2024-07-17 10:50:31 +0530

    Python

---
+++ List.py
@@ -0,0 +1,17 @@
+countries = ['India',True,'New Zeland','Dubai','Australia']
+countries2 = list(('U.S.A',6,False))
+
+print(type(countries))
+print(type(countries2))
+
+#we can store different type of data types in a LIST
+# print(countries);
+# countries[0] = 'Bangladesh'
+# print(countries)
+
+#indexing in PYHTON is : 0,1,2,3,4
+#other indexing is like :-5,-4,-3,-2,-1
+
+print(countries[-1])  #Least from the last
+
+print(type(countries[1]))
---
+++ Listmethods.py
@@ -0,0 +1,17 @@
+list1 = [5,8,6,3,1,2]
+list2 = list(('Malbro','Gold','Pine','S-lite'))
+
+# list1.extend(list2)
+# list2.append('Mild')  #added at the end of the given list
+# list2.insert(1,'Big-Gold') #for adding at a specific index
+# list2.remove('Malbro') #for removing any list element
+# list2.pop()  #for removing last element of the list
+# list2.pop(index) #for removing particular element on the list
+# list2.clear()  #For clearing or emptying the list
+# del list2  #For deleting the list and for indefine it
+# print(list2.count('Gold'))
+# print(list2.index('Pine'))
+
+# list1.sort() for arranging in ascending order
+# list2.reverse()
+# list3 = list1.copy() #for making new list which contains same thing as a previous lists
